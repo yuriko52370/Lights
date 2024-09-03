126dc69d86ea33c16503c1e5274e3db3c7c0a60d
yuriko53470 <162119385+yuriko52370@users.noreply.github.com>
2024-09-03 16:02:45 +0000

    [Fix]  app-conntroller, render edit/app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,21 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_authentication
 
- def after_sign_in_path_for(resource)
-    about_path
- end
+  private
 
- def after_sign_out_path_for(resource)
-    root_path
- end
-
- private
+  def configure_authentication
+    if admin_controller?
+      authenticate_admin!
+    else
+      authenticate_user! unless action_is_public?
+    end
+  end
 
   def admin_controller?
     self.class.module_parent_name == 'Admin'
   end
 
-protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  def action_is_public?
+    controller_name == 'homes' && action_name == 'top'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,18 +11,18 @@ Rails.application.routes.draw do
 
   scope module: :public do
     devise_for :users
-  
+
     root to: "homes#top"
     get "homes/about" => "homes#about", as: "about"
     get '/search', to: 'searches#search'
-  
+
     resources :posts, only: [:new, :create, :index, :show, :edit, :update] do
       resources :choice, only: [:new, :create, :index, :show, :edit, :update] do
         resource :favorite, only: [:create, :destroy]
       end
        resources :comments, only: [:create]
     end
-  
+
     resources :users, only: [ :edit, :mypage] do
       member do
         get :mypage
