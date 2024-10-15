216da8b4d19ddb6346d0880c909aea4c4b164de2
yuriko53470 <162119385+yuriko52370@users.noreply.github.com>
2024-10-15 14:23:06 +0000

    [Clear] devise-sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,13 @@ class ApplicationController < ActionController::Base
    before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
- def after_sign_in_path_for(resource)
-    about_path
- end
-
-  def after_sign_out_path_for(resource)
-    root_path
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email])
+  end
 
   def configure_authentication
     if admin_controller?
@@ -34,3 +26,4 @@ class ApplicationController < ActionController::Base
     controller_name == 'homes' && action_name == 'top'
   end
 end
+
